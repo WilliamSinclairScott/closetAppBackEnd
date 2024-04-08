06961964fd98a4bc4d2fa59be870f01a5c2f18f3
williamsinclairscott <william.sinclair.scott@gmail.com>
2024-04-08 10:32:12 -0400

    init

---
+++ models/user.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  userEmail: {
+    type: String,
+    validate: {
+      validator: async function(email) {
+        const user = await this.constructor.findOne({ email });
+        if(user) {
+          if(this.id === user.id) {
+            return true;
+          }
+          return false;
+        }
+        return true;
+      },
+      message: props => 'The specified email address is already in use.'
+    },
+    required: [true, 'User email required']
+  }
+})
