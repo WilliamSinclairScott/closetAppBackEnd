874ab3cbb913669ef746e52017bef83af8fb8dcb
williamsinclairscott <william.sinclair.scott@gmail.com>
2024-04-11 14:44:39 -0400

    update findUserByID

--- controllers/userController.js
+++ controllers/userController.js
@@ -70,7 +70,12 @@ export const updateUser = async (req, res) => {
 export const updateUserByUserID = async (req, res) => {
   try {
     const { userID } = req.params;
-    const updatedUser = await userModel.findOneAndUpdate({ userID })
+    const updatedUser = await userModel.findOneAndUpdate(
+      { name: userID },
+      { $addToSet: { closetItems: req.body.closetItems } },
+      { new: true }
+      ).populate('closetItems')
+      
     if (updatedUser) {
       res.json(updatedUser);
     } else {
